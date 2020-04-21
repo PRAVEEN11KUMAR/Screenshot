c80e8d4716215c75b81d0b4dac7dcde78b656352
B.Praveen Kumar <31960965+PRAVEEN11KUMAR@users.noreply.github.com>
2020-04-21 19:11:58 +0530

    Screenshot
    we can use tkinter & pyautogui modules in this task

---
+++ Screenshot.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import pyautogui
+
+win = tk.Tk()
+win.title("Take Screenshot")
+win.geometry('300x300')
+
+def takescreenshot():
+    screenshot = pyautogui.screenshot()
+    screenshot.save('F:\python\screenshot.png')
+
+button = tk.Button(win, text = "Capture Screenshot", command = takescreenshot)
+button.pack()
+
+win.mainloop()
